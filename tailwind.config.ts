1e8d8243932be5d8a8d3f68dd262acc919599d8b
Ricky Dodd <63101193+rickydodd@users.noreply.github.com>
2025-01-22 16:08:52 +1100

    add: design system, basic light and dark themes
    Add colours and font sizes from the design system into the TailwindCSS
    config, as well as CSS variables to facilitate basic light and dark
    themes, as set by system or browser settings.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors")
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,11 +9,51 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      background: "var(--background)",
+      foreground: "var(--foreground)",
+      option: "var(--option)",
+      subtitle: "var(--subtitle)",
+      "blue": "#abc1e1",
+      "green": "#26d782",
+      "grey": "#f4f6fa",
+      "navy": {
+        grey: "#626c7f",
+        DEFAULT: "#3b4d66",
+        dark: "#313e51",
       },
+      "purple": "#a729f5",
+      "red": "#ee5454",
+      "white": colors.white,
+    },
+    fontSize: {
+      "xs": ["0.85rem", {
+        lineHeight: "1.5",
+      }],
+      "sm": ["0.875", {
+        lineHeight: "1.5",
+      }],
+      "base": ["1rem", {
+        lineHeight: "1.5",
+      }],
+      "md": ["1.125rem", {
+        lineHeight: "1",
+      }],
+      "lg": ["1.25rem", {
+        lineHeight: "1.2",
+      }],
+      "xl": ["1.5rem", {
+        lineHeight: "1.2",
+      }],
+      "2xl": ["2.5rem", {
+        lineHeight: "1",
+      }],
+      "3xl": ["4rem", {
+        lineHeight: "1",
+      }],
+      "4xl": ["9rem", {
+        lineHeight: "1",
+      }],
     },
   },
   plugins: [],
