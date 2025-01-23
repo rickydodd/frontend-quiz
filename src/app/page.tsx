bf470cfac18131efd253247d863bf61ce5555b0e
Ricky Dodd <63101193+rickydodd@users.noreply.github.com>
2025-01-23 12:17:09 +1100

    add: read in and parse data, quiz type, nav
    This commit adds:
    - Type `Quiz`, describes the structure of the
    internal `Quizzes` in the data JSON file.
    - Reading and parsing data JSON file.
    - A navigation for accessing quizzes, which iterates the parsed data
      JSON file to build the index of quizzes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import { promises as fs } from "fs";
 
-export default function Home() {
+type Quiz = {
+  title: string;
+  icon: string;
+  color: string;
+  questions: {
+    question: string;
+    options: string[];
+    answer: string;
+  }[];
+};
+
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + "/src/app/data.json", "utf8");
+  const data = JSON.parse(file);
 
   return (
     <>
@@ -12,6 +26,28 @@ export default function Home() {
         <p className="text-xs italic text-subtitle">
           Pick a subject to get started.
         </p>
+        <nav className="mt-10 font-medium" aria-label="Quiz subjects">
+          {data.quizzes.map((quiz: Quiz) => (
+            <a
+              href={`/${quiz.title.toLowerCase()}`}
+              key={quiz.title}
+              className="mt-3 flex items-center gap-4 rounded-2xl bg-option px-3 py-2"
+            >
+              <div
+                aria-hidden="true"
+                className="w-10 rounded-md"
+                style={{ background: quiz.color }}
+              >
+                <img
+                  src={quiz.icon}
+                  alt={`${quiz.title} icon`}
+                  className="box-border p-1"
+                />
+              </div>
+              {quiz.title}
+            </a>
+          ))}
+        </nav>
       </main>
     </>
   );
