26d5e9e82e3f235a930c1e7aceb68b94dee71e56
Ricky Dodd <63101193+rickydodd@users.noreply.github.com>
2025-01-31 13:39:50 +1100

    refac: line height of h1 to match design spec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
         className="z-1 absolute top-0 h-screen w-screen bg-background-pattern bg-no-repeat"
       ></div>
       <main className="relative z-0 mx-6 mt-8">
-        <h1 className="mb-4 text-[clamp(2.5rem,5vw+1rem,4rem)] font-thin">
+        <h1 className="mb-4 text-[clamp(2.5rem,5vw+1rem,4rem)] font-thin leading-[1.2]">
           Welcome to the{" "}
           <span className="block font-medium">Frontend Quiz!</span>
         </h1>
